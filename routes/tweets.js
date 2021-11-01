e8685d41f0b8bb34a944ebc00d0408c40c997914
jto247 <jasonto247@hotmail.com>
2021-11-01 21:25:39 +1000

    Added socket.io

--- routes/tweets.js
+++ routes/tweets.js
@@ -10,6 +10,29 @@ const stemmer = require('natural').PorterStemmer;
 const analyzer = new Analyzer("English", stemmer, "afinn");
 const redis = require('redis');
 
+const { io } = require("socket.io-client");
+
+
+
+const socket = io("ws://localhost:3001");
+socket.on('connect', function() {
+    console.log('Connected to Stream Server');
+})
+
+
+socket.on('newTweet', (json) => {
+    console.log(json);
+
+    if (json.matching_rules[0].id == rulesID) {
+        //sentimental analysis on tweet
+        let output = Number(sentiment.sentimentalAnalysis(json.data.text.split(" ")));
+        //Get array of sentimental values for each term and save it to csv file
+        sentimentalValue = sentiment.updateCSV(searchTerm, Math.sign(output), sentimentalValue);
+    }
+
+})
+
+
 require('dotenv').config();
 const AWS = require('aws-sdk');
 
@@ -30,9 +53,6 @@ const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 let searchTerm ="";
 let rulesID = "";
 var sentimentalValue;
-
-const redisPort = '6379'; // Port
-const redisName = 'redis' // Name of Redis
 
 let stream = needle.get(streamURL, {
     headers: {
@@ -79,104 +99,8 @@ async function getAllRules() {
     return (response.body);
 }
 
-//Built upon from https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Filtered-Stream/filtered_stream.js
-async function deleteAllRules(rules) {
-
-    if (!Array.isArray(rules.data)) {
-        return null;
-    }
-
-    const ids = rules.data.map(rule => rule.id);
-
-    const data = {
-        "delete": {
-            "ids": ids
-        }
-    }
-
-    const response = await needle('post', rulesURL, data, {
-        headers: {
-            "content-type": "application/json",
-            "authorization": 'Bearer ' + twitterBearer
-        }
-    })
-
-    if (response.statusCode !== 200) {
-        throw new Error(response.body);
-    }
-
-    return (response.body);
-
-}
-
-//Built upon from https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Filtered-Stream/filtered_stream.js
-async function setRules(value) {
-
-    const data = {
-       'add': [{'value': value}]
-    }
-
-    const response = await needle('post', rulesURL, data, {
-        headers: {
-            "content-type": "application/json",
-            "authorization": 'Bearer ' + twitterBearer
-        }
-    })
-
-    if (response.statusCode !== 201) {
-        throw new Error(response.body);
-    }
-
-    return (response.body);
-
-}
-
-//Function to connect to stream
-//Retrieved from https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/b3e13798ae1093251f6b03830e4c30b5002e3c46/Filtered-Stream/filtered_stream.js#L145
-async function streamConnect(retryAttempt) {
-    //sentimentalValue = await sentiment.readCSV();
-    stream.on('data', data => {
-        try {
-            const json = JSON.parse(data);
-            console.log(json);
-            //console.log(json.matching_rules[0].id);
-            //console.log("Rules ID: " +rulesID);
-            if (json.matching_rules[0].id == rulesID) {
-                //console.log(rulesID + "++");
-                let output = Number(sentiment.sentimentalAnalysis(json.data.text.split(" ")));
-
-                //Get array of sentimental values for each term and save it to csv file
-                sentimentalValue = sentiment.updateCSV(searchTerm, Math.sign(output), sentimentalValue);
-            }
-            // A successful connection resets retry count.
-            retryAttempt = 0;
-        } catch (e) {
-            if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
-                console.log(data.detail);
-                process.exit(1)
-            } else {
-                // Keep alive signal received. Do nothing.
-            }
-        }
-    }).on('err', error => {
-        if (error.code !== 'ECONNRESET') {
-            console.log(error.code);
-            process.exit(1);
-        } else {
-            // This reconnection logic will attempt to reconnect when a disconnection is detected.
-            // To avoid rate limits, this logic implements exponential backoff, so the wait time
-            // will increase if the client cannot reconnect to the stream. 
-            setTimeout(() => {
-                console.warn("A connection error occurred. Reconnecting...")
-                streamConnect(++retryAttempt);
-            }, 2 ** retryAttempt)
-        }
-    });
-    return stream;
-
-}
-
 router.get('', async function(req,res) {
+    
     //destroy stream
     if (req.query.rules == "stopstream") {
         //save csv into redis and s3
@@ -232,6 +156,8 @@ router.get('', async function(req,res) {
         try {
             searchTerm = req.query.rules;
             sentimentalValue = sentiment.readCSV();
+            socket.emit('newRule', searchTerm);
+            await getAllRules();
 
             //Check Redis/other storage for the term
             client.hgetall(searchTerm, async function(err, result) {
@@ -249,25 +175,6 @@ router.get('', async function(req,res) {
                         
                         sentimentalValue = sentiment.readCSV();
                     }
-
-                    // Gets the complete list of rules currently applied to the stream
-                    let currentRules = await getAllRules();
-                    console.log(currentRules);
-                    //console.log("current rules: " + currentRules)
-            
-                    // Delete all rules. Comment the line below if you want to keep your existing rules.
-                    await deleteAllRules(currentRules);
-                    console.log("deleted all rules");
-            
-                    // Add rules to the stream. Comment the line below if you don't want to add new rules.
-                    await setRules(searchTerm);
-                    console.log("rules have been added to stream");
-
-                    currentRules = await getAllRules();
-                    //console.log("rules ID: " +currentRules);
-
-                    console.log("begin stream");
-                    streamConnect(0);
                 }
                 //Check S3 bucket
                 else {
@@ -294,70 +201,21 @@ router.get('', async function(req,res) {
                                 
                                 sentimentalValue = sentiment.readCSV();
                             }
-
-
-                            // Gets the complete list of rules currently applied to the stream
-                            let currentRules = await getAllRules();
-                            //console.log("current rules: " + currentRules)
-                    
-                            // Delete all rules. Comment the line below if you want to keep your existing rules.
-                            await deleteAllRules(currentRules);
-                            console.log("deleted all rules");
-                    
-                            // Add rules to the stream. Comment the line below if you don't want to add new rules.
-                            await setRules(searchTerm);
-                            console.log("rules have been added to stream");
-
-                            currentRules = await getAllRules();
-                            console.log(currentRules);
-
-                            console.log("begin stream");
-                            streamConnect(0);
                         } 
                         else {
-                            // Gets the complete list of rules currently applied to the stream
-                            let currentRules = await getAllRules();
-                            //console.log("current rules: " + currentRules)
-                    
-                            // Delete all rules. Comment the line below if you want to keep your existing rules.
-                            await deleteAllRules(currentRules);
-                            console.log("deleted all rules");
-                    
-                            // Add rules to the stream. Comment the line below if you don't want to add new rules.
-                            await setRules(searchTerm);
-                            console.log("rules have been added to stream");
 
-                            currentRules = await getAllRules();
-                            console.log("rules ID: " +currentRules);
-
-                            console.log("begin stream");
-                            streamConnect(0);
                         }
                     })
 
                 }
             });
-            // // Gets the complete list of rules currently applied to the stream
-            // let currentRules = await getAllRules();
-            // //console.log("current rules: " + currentRules)
-    
-            // // Delete all rules. Comment the line below if you want to keep your existing rules.
-            // await deleteAllRules(currentRules);
-            // console.log("deleted all rules");
-    
-            // // Add rules to the stream. Comment the line below if you don't want to add new rules.
-            // await setRules(searchTerm);
-            // console.log("rules have been added to stream");
     
         } catch (e) {
             console.error(e);
             process.exit(1);
         }
         res.redirect('/show?='+searchTerm);
-    
-        //Begin the stream
-        // console.log("begin stream");
-        // streamConnect(0);
+
     }
 })
 
